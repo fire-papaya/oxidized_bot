08d305503fd6281a0e588fdd94891a5394211af5
temur.navoi <temur.navoi@yandex.com>
2023-04-13 15:44:16 +0200

    add remaining command structs and draft impls

--- src/domain/command/handler/mod.rs
+++ src/domain/command/handler/mod.rs
@@ -1,1 +1,3 @@
 mod generate_code;
+mod upload_prime_image;
+mod upload_painted_image;
---
+++ src/domain/command/handler/upload_painted_image.rs
@@ -0,0 +1,23 @@
+use async_trait::async_trait;
+use crate::domain::command::CommandHandler;
+use crate::domain::handler::Handler;
+use crate::domain::command::entry_command::EntryCommand;
+use crate::domain::event::entry_event::EntryEvent;
+use crate::domain::error::EntryError;
+
+
+pub struct UploadPaintedImageCommand {}
+
+impl Handler<EntryCommand> for UploadPaintedImageCommand {
+    fn supports(handleable: EntryCommand) -> bool {
+        return handleable == EntryCommand::UploadPaintedImage
+    }
+}
+
+#[async_trait]
+impl CommandHandler<EntryCommand, EntryEvent, EntryError> for UploadPaintedImageCommand {
+    async fn handle(command: EntryCommand) -> Result<Vec<EntryEvent>, EntryError> {
+        todo!()
+    }
+}
+
